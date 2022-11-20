e38da469d772ed0805fda346cd13082679022f8e
selfint <selfint@gmail.com>
2022-11-20 21:15:22 +0200

    solve part 1

--- 2021/3/solution/src/main.rs
+++ 2021/3/solution/src/main.rs
@@ -1,3 +1,16 @@
+mod part_1;
+
+use std::io;
+
+use part_1::solve;
+
 fn main() {
-    println!("Hello, world!");
+    let input = io::stdin()
+        .lines()
+        .map(|l| l.expect("failed to get line from stdin"))
+        .map(|l| if l.is_empty() { None } else { Some(l) });
+
+    let solution = solve(input);
+
+    println!("solution = {}", solution);
 }
