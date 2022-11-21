1da720cccc2eb5995f25003f4229d70a0efbb3e7
selfint <40059156+selfint@users.noreply.github.com>
2022-11-21 18:00:09 +0200

    Solve 2021 day 4 (#4)

---
+++ 2021/4/solution/src/main.rs
@@ -0,0 +1,17 @@
+mod part_1;
+mod part_2;
+
+use std::io;
+
+use part_2::solve;
+
+fn main() {
+    let input = io::stdin()
+        .lines()
+        .map(|l| l.expect("failed to get line from stdin"))
+        .take_while(|l| l != "done");
+
+    let solution = solve(input);
+
+    println!("solution = {}", solution);
+}
