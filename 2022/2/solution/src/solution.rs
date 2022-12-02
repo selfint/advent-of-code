9d6b0b653d06bb5ff870bdb063b6af51505e6ff9
selfint <selfint@gmail.com>
2022-12-02 10:39:51 +0200

    solve part 2

--- 2022/2/solution/src/solution.rs
+++ 2022/2/solution/src/solution.rs
@@ -2,26 +2,38 @@ pub fn solve<I>(input: I) -> usize
 where
     I: Iterator<Item = String>,
 {
-    input.map(|r| score(&r)).sum()
+    input.map(|r| solve_round(&r)).map(|r| score(&r)).sum()
 }
 
 fn score(round: &str) -> usize {
-    let [opponent, me] = round.split_ascii_whitespace().collect::<Vec<_>>()[..] else {
-        panic!("failed to parse round");
-    };
-
-    match (opponent, me) {
-        ("A", "X") => 3 + 1,
-        ("A", "Y") => 6 + 2,
-        ("A", "Z") => 0 + 3,
-        ("B", "X") => 0 + 1,
-        ("B", "Y") => 3 + 2,
-        ("B", "Z") => 6 + 3,
-        ("C", "X") => 6 + 1,
-        ("C", "Y") => 0 + 2,
-        ("C", "Z") => 3 + 3,
-        _ => panic!("got unexpected move"),
+    match round {
+        "A X" => 3 + 1,
+        "A Y" => 6 + 2,
+        "A Z" => 0 + 3,
+        "B X" => 0 + 1,
+        "B Y" => 3 + 2,
+        "B Z" => 6 + 3,
+        "C X" => 6 + 1,
+        "C Y" => 0 + 2,
+        "C Z" => 3 + 3,
+        _ => panic!("got unexpected round"),
     }
+}
+
+fn solve_round(round: &str) -> String {
+    (match round {
+        "A X" => "A Z",
+        "A Y" => "A X",
+        "A Z" => "A Y",
+        "B X" => "B X",
+        "B Y" => "B Y",
+        "B Z" => "B Z",
+        "C X" => "C Y",
+        "C Y" => "C Z",
+        "C Z" => "C X",
+        _ => panic!("got unexpected round"),
+    })
+    .into()
 }
 
 #[cfg(test)]
@@ -38,12 +50,22 @@ mod tests {
 
         assert_eq!(expected_scores, actual_scores);
     }
+
+    #[test]
+    fn test_solve_round() {
+        let expected_solved_rounds = ["A X", "B X", "C X"];
+
+        let actual_solved_rounds = EXAMPLE_INPUT.map(solve_round);
+
+        assert_eq!(expected_solved_rounds, actual_solved_rounds);
+    }
+
     #[test]
     fn test_example() {
         let lines = EXAMPLE_INPUT.into_iter().map(|s| s.into());
 
         let solution = solve(lines);
 
-        assert_eq!(solution, 15);
+        assert_eq!(solution, 12);
     }
 }
