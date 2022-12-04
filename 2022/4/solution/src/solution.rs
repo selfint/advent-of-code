d094a2cb8768f58bb758b33887c1da3e45df953f
selfint <40059156+selfint@users.noreply.github.com>
2022-12-04 12:33:44 +0200

    2022-4 (#8)

---
+++ 2022/4/solution/src/solution.rs
@@ -0,0 +1,116 @@
+pub fn solve(input: impl Iterator<Item = String>) -> usize {
+    solve_part_2(input)
+}
+
+fn solve_part_1(input: impl Iterator<Item = String>) -> usize {
+    input
+        .map(|l| parse_ranges(&l))
+        .map(is_subrange)
+        .map(|r| r as usize)
+        .sum::<usize>()
+}
+
+fn solve_part_2(input: impl Iterator<Item = String>) -> usize {
+    input
+        .map(|l| parse_ranges(&l))
+        .map(has_overlap)
+        .map(|r| r as usize)
+        .sum::<usize>()
+}
+
+type RangeTuple = (usize, usize);
+
+fn parse_ranges(ranges: &str) -> (RangeTuple, RangeTuple) {
+    let (first_range, second_range) = ranges.split_once(',').expect("ranges didn't contain ','");
+
+    (parse_range(first_range), parse_range(second_range))
+}
+
+fn parse_range(range: &str) -> RangeTuple {
+    let (start, end) = range.split_once('-').expect("range didn't contain '-'");
+
+    (start.parse().unwrap(), end.parse().unwrap())
+}
+
+fn is_subrange(ranges: (RangeTuple, RangeTuple)) -> bool {
+    let (first_range, second_range) = ranges;
+    let (f_start, f_end) = first_range;
+    let (s_start, s_end) = second_range;
+
+    (f_start <= s_start && s_end <= f_end) || (s_start <= f_start && f_end <= s_end)
+}
+
+fn has_overlap(ranges: (RangeTuple, RangeTuple)) -> bool {
+    fn first_overlaps_second(f_start: usize, f_end: usize, s_start: usize, s_end: usize) -> bool {
+        (s_start <= f_start && f_start <= s_end) || (s_start <= f_end && f_end <= s_end)
+    }
+
+    let (first_range, second_range) = ranges;
+    let (f_start, f_end) = first_range;
+    let (s_start, s_end) = second_range;
+
+    first_overlaps_second(f_start, f_end, s_start, s_end)
+        || first_overlaps_second(s_start, s_end, f_start, f_end)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: [&str; 6] = [
+        "2-4,6-8", "2-3,4-5", "5-7,7-9", "2-8,3-7", "6-6,4-6", "2-6,4-8",
+    ];
+    const PART_1_ANSWER: usize = 2;
+    const PART_2_ANSWER: usize = 4;
+
+    fn iter_input() -> impl Iterator<Item = String> {
+        EXAMPLE_INPUT.into_iter().map(|s| s.into())
+    }
+
+    #[test]
+    fn test_parse_ranges() {
+        let expected_ranges = [
+            ((2, 4), (6, 8)),
+            ((2, 3), (4, 5)),
+            ((5, 7), (7, 9)),
+            ((2, 8), (3, 7)),
+            ((6, 6), (4, 6)),
+            ((2, 6), (4, 8)),
+        ];
+        let actual_ranges = EXAMPLE_INPUT.map(parse_ranges);
+
+        assert_eq!(expected_ranges, actual_ranges);
+    }
+
+    #[test]
+    fn test_is_subrange() {
+        let expected_results = [false, false, false, true, true, false];
+
+        let actual_results = EXAMPLE_INPUT.map(parse_ranges).map(is_subrange);
+
+        assert_eq!(expected_results, actual_results);
+    }
+
+    #[test]
+    fn test_has_overlap() {
+        let expected_results = [false, false, true, true, true, true];
+
+        let actual_results = EXAMPLE_INPUT.map(parse_ranges).map(has_overlap);
+
+        assert_eq!(expected_results, actual_results);
+    }
+
+    #[test]
+    fn test_part_1() {
+        let solution = solve_part_1(iter_input());
+
+        assert_eq!(solution, PART_1_ANSWER);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let solution = solve_part_2(iter_input());
+
+        assert_eq!(solution, PART_2_ANSWER);
+    }
+}
