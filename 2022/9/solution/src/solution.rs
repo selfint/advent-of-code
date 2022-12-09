cb90b08b6e94a4f70fa7978bbf8a769ccb3f8de5
selfint <selfint@gmail.com>
2022-12-09 17:39:49 +0100

    solve part 2

--- 2022/9/solution/src/solution.rs
+++ 2022/9/solution/src/solution.rs
@@ -2,10 +2,11 @@ use std::collections::HashSet;
 
 type Solution = usize;
 pub fn solve(input: impl Iterator<Item = String>) -> Solution {
-    solve_part_1(input)
+    solve_part_2(input)
 }
 
-type State = ((i32, i32), (i32, i32));
+type Knot = ((i32, i32), (i32, i32));
+type State = Vec<(i32, i32)>;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Step {
@@ -30,7 +31,26 @@ fn solve_part_1(input: impl Iterator<Item = String>) -> Solution {
 }
 
 fn solve_part_2(input: impl Iterator<Item = String>) -> Solution {
-    todo!()
+    let total_knots = 10;
+    let mut state = [(0, 0)].repeat(total_knots);
+    let mut tail_locations = HashSet::new();
+
+    for line in input {
+        for step in parse_step(&line) {
+            state[total_knots - 1] = update_head(step, state[total_knots - 1]);
+
+            for i in (1..total_knots).rev() {
+                let head = state[i];
+                let tail = state[i - 1];
+                let new_tail = update_tail((head, tail));
+                state[i - 1] = new_tail;
+            }
+
+            tail_locations.insert(state[0]);
+        }
+    }
+
+    tail_locations.len()
 }
 
 fn parse_step(step: &str) -> Vec<Step> {
@@ -47,17 +67,25 @@ fn parse_step(step: &str) -> Vec<Step> {
     [step].repeat(amount)
 }
 
-fn apply_step(state: State, step: Step) -> State {
-    let ((h_y, h_x), (t_y, t_x)) = state;
+fn apply_step(state: Knot, step: Step) -> Knot {
+    let (head, tail) = state;
+    let new_head = update_head(step, head);
+    let new_tail = update_tail((new_head, tail));
 
-    let (nh_y, nh_x) = match step {
+    (new_head, new_tail)
+}
+
+fn update_head(step: Step, (h_y, h_x): (i32, i32)) -> (i32, i32) {
+    match step {
         Step::Up => (h_y - 1, h_x),
         Step::Down => (h_y + 1, h_x),
         Step::Left => (h_y, h_x - 1),
         Step::Right => (h_y, h_x + 1),
-    };
+    }
+}
 
-    let new_tail = match (nh_y - t_y, nh_x - t_x) {
+fn update_tail(((h_y, h_x), (t_y, t_x)): Knot) -> (i32, i32) {
+    match (h_y - t_y, h_x - t_x) {
         // direct
         (0, 2) => (t_y, t_x + 1),
         (0, -2) => (t_y, t_x - 1),
@@ -73,12 +101,19 @@ fn apply_step(state: State, step: Step) -> State {
         (-1, 2) => (t_y - 1, t_x + 1),
         (1, -2) => (t_y + 1, t_x - 1),
         (-1, -2) => (t_y - 1, t_x - 1),
+        (2, 2) => (t_y + 1, t_x + 1),
+        (-2, 2) => (t_y - 1, t_x + 1),
+        (2, -2) => (t_y + 1, t_x - 1),
+        (-2, -2) => (t_y - 1, t_x - 1),
 
         // distance is ok
-        _ => (t_y, t_x),
-    };
+        (0, 0) | (1, 0) | (0, 1) | (1, 1) | (-1, 0) | (0, -1) | (-1, -1) | (1, -1) | (-1, 1) => {
+            (t_y, t_x)
+        }
 
-    ((nh_y, nh_x), new_tail)
+        // illegal
+        _ => panic!("illegal state: ({h_y}, {h_x}) ({t_y}, {t_x})"),
+    }
 }
 
 #[cfg(test)]
@@ -95,7 +130,7 @@ mod tests {
     }
 
     fn iter_input_2() -> impl Iterator<Item = String> {
-        EXAMPLE_INPUT.into_iter().map(|s| s.into())
+        EXAMPLE_INPUT_2.into_iter().map(|s| s.into())
     }
 
     #[test]
