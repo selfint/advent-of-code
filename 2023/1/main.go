a8052e269abcfc467094b52676dbcbdef8df578c
selfint <40059156+selfint@users.noreply.github.com>
2023-12-08 23:28:50 +0000

    2023/1: get input

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func getInput() ([]string, error) {
+	scanner := bufio.NewScanner(os.Stdin)
+	var lines []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading standard input:", err)
+		return lines, err
+	}
+
+	return lines, nil
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	input, err := getInput()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading standard input:", err)
+		return
+	}
+
+	for _, line := range input {
+		fmt.Println(line)
+	}
 }
