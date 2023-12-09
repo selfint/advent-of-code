330f14086ca26a60098bef0826f7960f6704f48e
selfint <40059156+selfint@users.noreply.github.com>
2023-12-09 15:44:21 +0000

    2023/2: solve part 1

---
+++ 2023/2/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func getInput() ([]string, error) {
+	scanner := bufio.NewScanner(os.Stdin)
+	lines := []string{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading standard input:", err)
+		return lines, err
+	}
+
+	return lines, nil
+}
+
+type Game struct {
+	maxRed   int
+	maxGreen int
+	maxBlue  int
+}
+
+func parseGame(game string) (Game, error) {
+	parts := strings.Split(game, ":")
+	if len(parts) != 2 {
+		return Game{}, fmt.Errorf("invalid game string: %s", game)
+	}
+
+	rawGrabs := parts[1]
+
+	maxRed, maxGreen, maxBlue := 0, 0, 0
+
+	for _, rawGrab := range strings.Split(rawGrabs, ";") {
+		for _, rawHandful := range strings.Split(rawGrab, ",") {
+			parts := strings.Split(rawHandful, " ")
+			if len(parts) != 3 {
+				return Game{}, fmt.Errorf("invalid handful '%s': parts=%v", rawHandful, parts)
+			}
+
+			rawAmount, rawColor := parts[1], parts[2]
+			amount, err := strconv.Atoi(rawAmount)
+			if err != nil {
+				return Game{}, fmt.Errorf("invalid amount: %s", rawAmount)
+			}
+
+			switch rawColor {
+			case "red":
+				if amount > maxRed {
+					maxRed = amount
+				}
+			case "green":
+				if amount > maxGreen {
+					maxGreen = amount
+				}
+			case "blue":
+				if amount > maxBlue {
+					maxBlue = amount
+				}
+			default:
+				return Game{}, fmt.Errorf("invalid color: %s", rawColor)
+			}
+		}
+	}
+
+	return Game{maxRed, maxGreen, maxBlue}, nil
+}
+
+func main() {
+	input, err := getInput()
+	if err != nil {
+		log.Println("Error reading standard input:", err)
+		return
+	}
+
+	maxRed, maxGreen, maxBlue := 12, 13, 14
+	sum := 0
+
+	for i, rawGame := range input {
+		gameId := i + 1
+		game, err := parseGame(rawGame)
+		if err != nil {
+			log.Printf("Failed to parse line %d '%s': %v\n", i, rawGame, err)
+			os.Exit(1)
+		}
+
+		if game.maxRed > maxRed {
+			continue
+		}
+
+		if game.maxGreen > maxGreen {
+			continue
+		}
+
+		if game.maxBlue > maxBlue {
+			continue
+		}
+
+		sum += gameId
+	}
+
+	fmt.Println(sum)
+}
